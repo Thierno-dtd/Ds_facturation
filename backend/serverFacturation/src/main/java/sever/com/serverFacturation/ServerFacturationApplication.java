be8cbce0260fc23e0a2458ef8aa399c77afb6500
Thierno-dtd <davidouthe2@gmail.com>
2024-01-20 09:10:38 +0000

    initialisation du projet

---
+++ backend/serverFacturation/src/main/java/sever/com/serverFacturation/ServerFacturationApplication.java
@@ -0,0 +1,13 @@
+package sever.com.serverFacturation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerFacturationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerFacturationApplication.class, args);
+	}
+
+}
---
+++ backend/serverFacturation/src/test/java/sever/com/serverFacturation/ServerFacturationApplicationTests.java
@@ -0,0 +1,13 @@
+package sever.com.serverFacturation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerFacturationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
