31792a844192f573726929c4562488345ff0b2fa
Thierno-dtd <davidouthe2@gmail.com>
2024-01-23 23:28:50 +0000

    coorections sur UserDto et initialisation du front

--- backend/serverFacturation/src/main/java/sever/com/serverFacturation/dtos/UserDto.java
+++ backend/serverFacturation/src/main/java/sever/com/serverFacturation/dtos/UserDto.java
@@ -1,12 +1,6 @@
 package sever.com.serverFacturation.dtos;
 
-import com.flutter.project_flutter.constants.TypeRoles;
-import com.flutter.project_flutter.entites.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
---
+++ frontend/facturationFront/src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'facturationFront'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('facturationFront');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('facturationFront app is running!');
+  });
+});
